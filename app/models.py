06fc0e96f5a088e6003e0bf9c01d775c4a4a2b36
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-12-09 16:58:29 -0500

    Add db to gitignore + lint fixes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db
 
+
 class User(db.Model):
     uuid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +11,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
 
 class Post(db.Model):
     uuid = db.Column(db.Integer, primary_key=True)
